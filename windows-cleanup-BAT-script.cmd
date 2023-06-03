affa94b80df037a9e5c5906af287a90f2e4d376f
Pablo-Contreras-Rosado <134799165+Pablo-Contreras-Rosado@users.noreply.github.com>
2023-06-03 21:06:53 +0200

    Added new directories and a way to clear Firefox cache

--- windows-cleanup-BAT-script.cmd
+++ windows-cleanup-BAT-script.cmd
@@ -16,18 +16,27 @@ set LIST_TO_DELETE=(^
     "C:\Windows\Installer\$PatchCache$"^
     "C:\Windows\SoftwareDistribution\Download"^
     "C:\Windows\Temp"^
+    "C:\Users\%USERMAME%\AppData\Local\Adobe\AcroCef\DC\Acrobat\Cache"^
+    "C:\Users\%USERNAME%\AppData\Local\Cache"^
 	"C:\Users\%USERNAME%\AppData\Local\CrashDumps"^
 	"C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Cache"^
 	"C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Code Cache"^
 	"C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Service Worker\CacheStorage"^
+    "C:\Users\%USERNAME%\AppData\Local\setup\cache"^
     "C:\Users\%USERNAME%\AppData\Local\Temp"^
 	"C:\Users\%USERNAME%\AppData\Local\Microsoft\Edge\User Data\Default\Cache"^
+    "C:\Users\%USERNAME%\AppData\Local\Microsoft\Edge\User Data\Default\Code Cache"^
+	"C:\Users\%USERNAME%\AppData\Local\Microsoft\Edge\User Data\Default\Service Worker\CacheStorage"^
+    "C:\Users\%USERNAME%\AppData\Local\Microsoft\FontCache"^
 	"C:\Users\%USERNAME%\AppData\Local\Microsoft\Internet Explorer\CacheStorage"^
 	"C:\Users\%USERNAME%\AppData\Local\Microsoft\Office\16.0\Lync\Tracing"^
 	"C:\Users\%USERNAME%\AppData\Local\Microsoft\Office\16.0\Wef\"^
+    "C:\Users\%USERNAME%\AppData\Local\Microsoft\Terminal Server Client\Cache"^
 	"C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\INetCache"^
 	"C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\Temporary Internet Files"^
 	"C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\WebCache"^
+    "C:\Users\%USERNAME%\AppData\Local\WebEx\wbxcache"^
+	"C:\Users\%USERNAME%\AppData\LocalLow\Adobe\AcroCef\DC\Acrobat\Cache\Cache"^
     "C:\Users\%USERNAME%\AppData\LocalLow\Temp"^
     "C:\Users\%USERNAME%\AppData\Roaming\Temp"^
 )
@@ -37,6 +46,31 @@ for %%a in %LIST_TO_DELETE% do (
         for /D %%p in (%%a) do rmdir "%%p" /S /Q > NUL 2> NUL
         del %%a\* /F /Q /S > NUL 2> NUL
     )
+)
+
+REM This clears Firefox cache
+cd /d "C:\Users\%USERNAME%\AppData\Local\Mozilla\Firefox\Profiles"
+
+for /d %%a in (*.default) do (
+	if exist %%a (
+		for /D %%p in (%%a\cache2) do rmdir "%%p" /S /Q > NUL 2> NUL
+		del %%a\cache2\* /F /Q /S > NUL 2> NUL
+		for /D %%p in (%%a\startupCache) do rmdir "%%p" /S /Q > NUL 2> NUL
+		del %%a\startupCache\* /F /Q /S > NUL 2> NUL
+		for /D %%p in (%%a\jumpListCache) do rmdir "%%p" /S /Q > NUL 2> NUL
+		del %%a\jumpListCache\* /F /Q /S > NUL 2> NUL
+	)
+)
+
+for /d %%a in (*.default-release) do (
+	if exist %%a (
+		for /D %%p in (%%a\cache2) do rmdir "%%p" /S /Q > NUL 2> NUL
+		del %%a\cache2\* /F /Q /S > NUL 2> NUL
+		for /D %%p in (%%a\startupCache) do rmdir "%%p" /S /Q > NUL 2> NUL
+		del %%a\startupCache\* /F /Q /S > NUL 2> NUL
+		for /D %%p in (%%a\jumpListCache) do rmdir "%%p" /S /Q > NUL 2> NUL
+		del %%a\jumpListCache\* /F /Q /S > NUL 2> NUL
+	)
 )
 
 echo All done, you can close the script
